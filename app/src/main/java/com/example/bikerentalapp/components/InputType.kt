a3baf8a64c57bce2f510f542976d9c965203da17
Pham Viet Hoa <phamviethoa2416@gmail.com>
2024-10-24 05:32:50 +0700

    revert commit

--- app/src/main/java/com/example/bikerentalapp/components/InputType.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bikerentalapp.components
-
-sealed class InputType {
-    data object Text: InputType()
-    data object Phone: InputType()
-    data object Email: InputType()
-    data object Password: InputType()
-}
