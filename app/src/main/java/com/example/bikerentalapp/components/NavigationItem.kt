c1358bac0ac8dccc6f07be51e630a7b2cb899f9b
Pham Viet Hoa <phamviethoa2416@gmail.com>
2024-11-06 09:16:52 +0700

    feat: add navigation items for bottom app bar

---
+++ app/src/main/java/com/example/bikerentalapp/components/NavigationItem.kt
@@ -0,0 +1,37 @@
+package com.example.bikerentalapp.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person3
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavigationItem (
+    val label: String,
+    val icon: ImageVector,
+    val route: String
+)
+
+val navigationItems = listOf(
+    NavigationItem(
+        label = "Trang chủ",
+        icon = Icons.Default.Home,
+        route = "home"
+    ),
+    NavigationItem(
+        label = "Trạm xe",
+        icon = Icons.Default.LocationOn,
+        route = "station"
+    ),
+    NavigationItem(
+        label = "Thông báo",
+        icon = Icons.Default.Notifications,
+        route = "notification"
+    ),
+    NavigationItem(
+        label = "Hồ sơ",
+        icon = Icons.Default.Person3,
+        route = "profile"
+    )
+)
