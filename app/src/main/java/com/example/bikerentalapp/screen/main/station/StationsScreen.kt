b10a2430c6de35fe5844c2c4442d5c485bfcb3c9
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-11-09 23:07:58 +0700

    refactor : request permission when user open map
    (cherry picked from commit cc7e3d5d6db52e5395dedea1460963ccdf68fcc4)

---
+++ app/src/main/java/com/example/bikerentalapp/screen/main/station/StationsScreen.kt
@@ -0,0 +1,49 @@
+package com.example.bikerentalapp.screen.main.station
+
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.rememberCameraPositionState
+
+@Composable
+fun StationsScreen() {
+    val mapViewModel = remember { GoogleMapViewModel() }
+    val permissionState by mapViewModel.locationPermissionGranted.collectAsState()
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        mapViewModel.setLocationPermissionGranted(isGranted)
+    }
+    var isMapLoaded by remember { mutableStateOf(false) }
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition(LatLng(21.043303, 105.793921), 12f, 0f, 0f)
+    }
+// The screen with map of bike stations (Refer Trạm xe on TNGo)
+    if(permissionState){
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            onMapLoaded = { isMapLoaded = true },
+            cameraPositionState = cameraPositionState,
+            uiSettings = MapUiSettings(
+                zoomControlsEnabled = false,
+                compassEnabled = false
+            ),
+            properties = MapProperties(isMyLocationEnabled = true),
+        )
+    }else SideEffect{
+        launcher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+}
