518c3884ce050f2e345b0862592a673a5ee0a85d
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-11-19 18:09:07 +0700

    refactor : remove stationList and add methods to search station
    (cherry picked from commit a231d8a1fd7562ec6c74ad3e3fff21daff26cdfe)

--- app/src/main/java/com/example/bikerentalapp/screen/main/station/StationViewModel.kt
+++ app/src/main/java/com/example/bikerentalapp/screen/main/station/StationViewModel.kt
@@ -2,40 +2,59 @@ package com.example.bikerentalapp.screen.main.station
 
 import androidx.lifecycle.ViewModel
 import com.example.bikerentalapp.model.Station
+import com.example.bikerentalapp.model.stations
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import java.util.Locale
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class GoogleMapViewModel : ViewModel() {
 
     //Example of list of stations
-    val stationList = listOf(
-        Station(21.043303, 105.793921, 1, 10),
-        Station(21.045000, 105.795000, 2, 20),
-        Station(21.046000, 105.796000, 3, 30),
-        Station(21.047000, 105.797000, 4, 40),
-        Station(21.048000, 105.798000, 5, 50),
-        Station(21.049000, 105.799000, 6, 60),
-        Station(21.050000, 105.800000, 7, 70),
-        Station(21.051000, 105.801000, 8, 80),
-        Station(21.052000, 105.802000, 9, 90),
-        Station(21.053000, 105.803000, 10, 100),
-        Station(21.054000, 105.804000, 11, 110),
-        Station(21.055000, 105.805000, 12, 120),
-        Station(21.056000, 105.806000, 13, 130),
-        Station(21.057000, 105.807000, 14, 140),
-        Station(21.058000, 105.808000, 15, 150),
-        Station(21.059000, 105.809000, 16, 160),
-        Station(21.060000, 105.810000, 17, 170),
-        Station(21.061000, 105.811000, 18, 180),
-        Station(21.062000, 105.812000, 19, 190),
-        Station(21.063000, 105.813000, 20, 200)
-    )
-
+    val stationList = stations
     private val _locationPermissionGranted = MutableStateFlow(false)
     val locationPermissionGranted = _locationPermissionGranted.asStateFlow()
+    private val _searchState = MutableStateFlow(SearchStationState())
+    val searchState = _searchState.asStateFlow()
 
     fun setLocationPermissionGranted(granted: Boolean) {
         _locationPermissionGranted.value = granted
     }
 
+    fun searchStation(query: String) {
+        val filteredStations = stations.filter {
+            it.id.lowercase(Locale.getDefault()).contains(query)
+        }
+
+        if (query == "") {
+            _searchState.value = SearchStationState(null)
+        } else {
+            _searchState.update {
+                it.copy(stationList = filteredStations)
+            }
+        }
+
+    }
+
+    fun selectDevice(selectedDevice: Station,cameraPositionState: CameraPositionState,coroutineScope: CoroutineScope) {
+        coroutineScope.launch {
+            cameraPositionState.animate(
+                update = CameraUpdateFactory.newLatLngZoom(
+                    LatLng(selectedDevice.lat, selectedDevice.lng),
+                    17f
+                ),
+                durationMs = 1000,
+            )
+        }
+        _searchState.value = SearchStationState(null)
+    }
 }
+
+data class SearchStationState(
+    val stationList: List<Station>? = null
+)
