1a1075590f00d9696f7df936e3c76755cf5c7f63
Minh Hai Hoang <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-12-06 14:07:23 +0700

    feat: create nested graph system
    (cherry picked from commit e0386817ecfd35612efa499a859afd1c0c86674c)

--- app/src/main/java/com/example/bikerentalapp/screen/main/MainScreen.kt
+++ app/src/main/java/com/example/bikerentalapp/screen/main/MainScreen.kt
@@ -1,6 +1,5 @@
 package com.example.bikerentalapp.screen.main
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
