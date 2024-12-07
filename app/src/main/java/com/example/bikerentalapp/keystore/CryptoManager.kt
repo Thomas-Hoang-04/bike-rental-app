f66f466e71ccae6943d399f672860e5f2e120118
Minh Hai Hoang <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-12-08 00:49:03 +0700

    dev: test keystore support for encrypted local storage
    (cherry picked from commit f29951c736437b5a900cc48abe51d00abdfd52c8)

---
+++ app/src/main/java/com/example/bikerentalapp/keystore/CryptoManager.kt
@@ -0,0 +1,86 @@
+package com.example.bikerentalapp.keystore
+
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import android.util.Base64
+import android.util.Log
+import java.security.*
+import java.security.spec.MGF1ParameterSpec
+import javax.crypto.Cipher
+import javax.crypto.spec.OAEPParameterSpec
+import javax.crypto.spec.PSource
+
+class CryptoManager {
+    private val keystore = KeyStore.getInstance("AndroidKeyStore").apply {
+        load(null)
+    }
+
+    private val encryptCipher = Cipher.getInstance(TRANSFORMATION).apply {
+        init(Cipher.ENCRYPT_MODE, getPublicKey(), OAEPParam)
+    }
+
+    private val decryptCipher = Cipher.getInstance(TRANSFORMATION).apply {
+        init(Cipher.DECRYPT_MODE, getPrivateKey(), OAEPParam)
+    }
+
+    private fun generateRSAKey() {
+        val keyPairGenerator: KeyPairGenerator
+            = KeyPairGenerator.getInstance(ALGORITHM, "AndroidKeyStore")
+
+        val keyGenSpec = KeyGenParameterSpec.Builder(
+            ALIAS,
+            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+        ).setBlockModes(BLOCK_MODE)
+         .setDigests(DIGEST)
+         .setEncryptionPaddings(PADDING)
+         .setKeySize(2048)
+         .setUserAuthenticationRequired(false)
+         .setRandomizedEncryptionRequired(true)
+         .build()
+
+        keyPairGenerator.initialize(keyGenSpec)
+        keyPairGenerator.generateKeyPair()
+    }
+
+    private fun getKeyEntry(): KeyStore.PrivateKeyEntry {
+        if (!keystore.containsAlias(ALIAS)) {
+            generateRSAKey()
+        }
+        return keystore.getEntry(ALIAS, null) as KeyStore.PrivateKeyEntry
+    }
+
+    private fun getPublicKey(): PublicKey {
+        val entry = getKeyEntry()
+        return entry.certificate.publicKey
+    }
+
+    private fun getPrivateKey(): PrivateKey {
+        val entry = getKeyEntry()
+        return entry.privateKey
+    }
+
+    fun encrypt(data: String): String {
+        val encrypted = encryptCipher.doFinal(data.encodeToByteArray())
+        return Base64.encodeToString(encrypted, Base64.URL_SAFE)
+    }
+
+    fun decrypt(data: String): String {
+        val decoded = Base64.decode(data, Base64.URL_SAFE)
+        return decryptCipher.doFinal(decoded).decodeToString()
+    }
+
+    companion object {
+        private const val ALIAS = "secret"
+        private const val ALGORITHM = KeyProperties.KEY_ALGORITHM_RSA
+        private const val BLOCK_MODE = KeyProperties.BLOCK_MODE_ECB
+        private const val DIGEST = KeyProperties.DIGEST_SHA256
+        private const val PADDING = KeyProperties.ENCRYPTION_PADDING_RSA_OAEP
+        private const val TRANSFORMATION = "$ALGORITHM/$BLOCK_MODE/$PADDING"
+        private val OAEPParam = OAEPParameterSpec(
+            "SHA-256",
+            "MGF1",
+            MGF1ParameterSpec.SHA1,
+            PSource.PSpecified.DEFAULT
+        )
+    }
+}
---
+++ app/src/main/java/com/example/bikerentalapp/screen/test/TestScreen.kt
@@ -0,0 +1,61 @@
+package com.example.bikerentalapp.screen.test
+
+import android.content.Context
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.bikerentalapp.keystore.CryptoManager
+import com.example.bikerentalapp.ui.theme.BikeRentalAppTheme
+import java.io.File
+import java.security.KeyStore
+
+@Composable
+fun TestScreen(context: Context) {
+    val cryptoManager = CryptoManager()
+
+    BikeRentalAppTheme {
+        var messageToEncrypt by remember { mutableStateOf("") }
+        var encryptedMessage by remember { mutableStateOf("") }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp)
+        ) {
+            TextField(
+                value = messageToEncrypt,
+                onValueChange = { messageToEncrypt = it },
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = { Text("Message to encrypt") }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Row {
+                Button(onClick = {
+                    val file = File(context.filesDir, "secret.txt")
+                    if (!file.exists()) {
+                        file.createNewFile()
+                    }
+                    val data = cryptoManager.encrypt(messageToEncrypt)
+                    file.writeText(data)
+                    encryptedMessage = data
+                }) {
+                    Text("Encrypt")
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+                Button(onClick = {
+                    val file = File(context.filesDir, "secret.txt")
+                    val data = file.readText()
+                    messageToEncrypt = cryptoManager.decrypt(data)
+                }) {
+                    Text("Decrypt")
+                }
+            }
+            Text(text = encryptedMessage)
+        }
+    }
+}
